c71f4918a85f829972090413322d9991df81dd47
Barry1138 <155800934+Barry1138@users.noreply.github.com>
2024-01-19 16:12:38 -0500

    Python Codes

---
+++ Python/arithmetic.py
@@ -0,0 +1,15 @@
+a = 8
+b = 2
+
+print('Addition:\t\t' , a, '+', b, '=', a + b)
+
+print('Subtraction:\t\t' , a, '-', b, '=', a - b)
+
+print('Multiplication:\t\t' , a, '*', b, '=', a * b)
+
+print('Division:\t\t' , a, '/', b, '=', a / b)
+print('Floor Division:\t\t' , a, '/', b, '=', a // b)
+
+print('Remainder:\t\t' , a, '%', b, '=', a % b)
+
+print('Exponent:\t\t' , a, '**', b, '=', a ** b, sep = '')
---
+++ Python/runtime.py
@@ -0,0 +1,2 @@
+title = 'Atlas Shrugged'
+print(title)

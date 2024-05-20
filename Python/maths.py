5c745b949b8352be0af96e404c5d2a72fa96eae7
Barry Calligan <155800934+Barry1138@users.noreply.github.com>
2024-05-19 22:46:53 -0400

    Add files via upload

---
+++ Python/maths.py
@@ -0,0 +1,13 @@
+import math, random
+
+print('Rounded Up 9.5:', math.ceil(9.5))
+print('Rounded Down 9.5:', math.floor(9.5))
+
+num = 4
+
+print('Squared:', math.pow(num, 2))
+print('Square Root:', math.sqrt(num))
+
+nums = random.sample(range(1, 60), 6)
+
+print = ('Your Lucky Lotto Numbers Are:', nums)
